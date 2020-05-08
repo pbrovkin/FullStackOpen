8ef669d4263b3efdb6fdddc75a46f141c81b9779
pbrovkin <pbrovkin@gmail.com>
2020-05-08 12:10:52 +0300

    unicafe, step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,30 +7,60 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
-const Statistic = ({ text, type }) => (
+const Feedback = ({ type, text }) => (
   <p>{text}: {type}</p>
 )
+
+const Average = ({ good, bad, all }) => {
+  if (all === 0) {
+    return <p>average: 0</p>
+  }
+  return (
+    <p>average: {(good - bad) / all}</p>
+  )
+}
+
+const Positive = ({ good, all }) => {
+  if (all === 0) {
+    return <p>positive: 0</p>
+  }
+  return <p>positive: {((100 * good) / all) + ' %'}</p>
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <Button onClick={handleGood} text='good' />
-      <Button onClick={handleNeutral} text='netral' />
-      <Button onClick={handleBad} text='bad' />
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='netral' />
+      <Button onClick={handleBadClick} text='bad' />
       <h1>Statistics</h1>
-      <Statistic type={good} text='good' />
-      <Statistic type={neutral} text='neutral' />
-      <Statistic type={bad} text='bad' />
+      <Feedback type={good} text='good' />
+      <Feedback type={neutral} text='neutral' />
+      <Feedback type={bad} text='bad' />
+      <Feedback type={all} text='all' />
+      <Average good={good} bad={bad} all={all} />
+      <Positive good={good} all={all} />
     </div>
   )
 }
