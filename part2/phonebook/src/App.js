ef0175da6d62b69c7565d2904bf32884d10b15a9
pbrovkin <pbrovkin@gmail.com>
2020-05-13 14:37:41 +0300

    phonebook, step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -32,7 +32,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person =>
+        <p key={person.name}>{person.name}</p>
+      )}
     </div>
   )
 }
