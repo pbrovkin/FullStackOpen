bf48b2a5c8e124a662551905195c3350326d59e2
pbrovkin <pbrovkin@gmail.com>
2020-06-10 13:31:53 +0300

    bloglist tests, step1

--- part4/bloglist/utils/logger.js
+++ part4/bloglist/utils/logger.js
@@ -1,5 +1,11 @@
-const info = (...params) => {
+/* const info = (...params) => {
     console.log(...params)
+} */
+
+const info = (...params) => {
+    if (process.env.NODE_ENV !== 'test') {
+        console.log(...params)
+    }
 }
 
 const error = (...params) => {
