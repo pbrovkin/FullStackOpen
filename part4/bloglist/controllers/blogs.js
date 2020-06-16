3aac222ee515c3bf12345300d109f5815eaf11fc
pbrovkin <pbrovkin@gmail.com>
2020-06-16 17:49:28 +0300

    bloglist tests, step5

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,27 +1,31 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
+})
+
+blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    })
+
+    if (body.title || body.url) {
+        const savedBlog = await blog.save()
+        response.json(savedBlog.toJSON())
+    } else {
+        response.status(400).json({
+            error: 'title or url missing'
         })
+    }
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-blogsRouter.delete('/:id', async (request, response) => {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-})
 
 module.exports = blogsRouter
--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -7,115 +7,101 @@ const api = supertest(app)
 
 const Blog = require('../models/blog')
 
-beforeEach(async () => {
-    await Blog.deleteMany({})
+describe('when there is initially some blogs saved', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
 
-    const blogObjects = helper.initialBlogs
-        .map(blog => new Blog(blog))
-    const promiseArray = blogObjects.map(blog => blog.save())
-    await Promise.all(promiseArray)
-})
+        const blogObjects = helper.initialBlogs
+            .map(blog => new Blog(blog))
+        const promiseArray = blogObjects.map(blog => blog.save())
+        await Promise.all(promiseArray)
+    })
+
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('app returns the correct amount of blogs', async () => {
+        const blogs = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        expect(blogs.body.length).toBe(helper.initialBlogs.length)
+    })
+
+    test('identifier property of the blog is named id', async () => {
+        const blogs = await api.get('/api/blogs')
+        blogs.body.map(blog => expect(blog.id).toBeDefined())
+    })
 
 
-test('app returns the correct amount of blogs', async () => {
-    const blogs = await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-
-    expect(blogs.body.length).toBe(helper.initialBlogs.length)
-})
-
-
-test('identifier property of the blog is named id', async () => {
-    const blogs = await api.get('/api/blogs')
-    blogs.body.map(blog => expect(blog.id).toBeDefined())
-})
-
-
-test('request creates a new blog post', async () => {
-    const newBlog = {
-        title: 'React patterns',
-        author: 'Michael Chan',
-        url: 'https://reactpatterns.com/',
-        likes: 15
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    const blogs = await api.get('/api/blogs')
-
-    expect(blogs.body.length).toBe(helper.initialBlogs.length + 1)
-
-    expect(blogs.body).toEqual(
-        expect.arrayContaining([
-            expect.objectContaining({
+    describe('addition of a new blog', () => {
+        test('request creates a new blog post', async () => {
+            const newBlog = {
                 title: 'React patterns',
                 author: 'Michael Chan',
                 url: 'https://reactpatterns.com/',
                 likes: 15
-            })
-        ])
-    )
-})
+            }
 
+            await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
 
-test('likes property has 0 value by default', async () => {
-    const newBlog = {
-        title: 'Type wars',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/'
-    }
+            const blogs = await api.get('/api/blogs')
 
-    const blogs = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+            expect(blogs.body.length).toBe(helper.initialBlogs.length + 1)
 
-    expect(blogs.body.likes).toEqual(0)
-})
+            expect(blogs.body).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        title: 'React patterns',
+                        author: 'Michael Chan',
+                        url: 'https://reactpatterns.com/',
+                        likes: 15
+                    })
+                ])
+            )
+        })
 
+        test('likes property has 0 value by default', async () => {
+            const newBlog = {
+                title: 'Type wars',
+                author: 'Robert C. Martin',
+                url: 'http://blog.cleancoder.com/'
+            }
 
-test('fails with status code 400 if title and url are missing', async () => {
+            const blogs = await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
 
-    const newBlog = {
-        author: 'Edsger W. Dijkstra',
-        likes: 12
-    }
+            expect(blogs.body.likes).toEqual(0)
+        })
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-})
+        test('fails with status code 400 if title and url are missing', async () => {
+            const newBlog = {
+                author: 'Goose Hiddink'
+            }
 
+            await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(400)
 
-describe('deletion of a blog', () => {
-    test('succeeds with status code 204 if id is valid', async () => {
-        const blogsAtStart = await helper.blogsInDb()
-        const blogToDelete = blogsAtStart[0]
+            const blogsAtEnd = await helper.blogsInDb()
 
-        await api
-            .delete(`/api/blogs/${blogToDelete.id}`)
-            .expect(204)
-
-        const blogsAtEnd = await helper.blogsInDb()
-
-        expect(blogsAtEnd.length).toBe(
-            helper.initialBlogs.length - 1
-        )
-
-        const titles = blogsAtEnd.map(b => b.title)
-
-        expect(titles).not.toContain(blogToDelete.title)
+            expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
+        })
     })
 })
-
 
 afterAll(() => {
     mongoose.connection.close()
