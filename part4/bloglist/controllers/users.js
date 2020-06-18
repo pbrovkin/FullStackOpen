0399d61f91accf6b48490f0555acd320d4aee9de
pbrovkin <pbrovkin@gmail.com>
2020-06-18 14:47:10 +0300

    bloglist expansions, step4

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -2,6 +2,11 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users.map(u => u.toJSON()))
+})
+
 usersRouter.post('/', async (request, response) => {
     const body = request.body
 
@@ -19,9 +24,5 @@ usersRouter.post('/', async (request, response) => {
     response.json(savedUser)
 })
 
-usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
-    response.json(users.map(u => u.toJSON()))
-})
 
 module.exports = usersRouter
