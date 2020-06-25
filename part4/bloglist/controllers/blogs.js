8dbc8f41659e1a6623ec1172286fb15cf953b7f6
pbrovkin <pbrovkin@gmail.com>
2020-06-25 17:10:25 +0300

    bloglist expansions, step9

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,13 +3,13 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
+/* const getTokenFrom = request => {
     const authorization = request.get('authorization')
     if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
         return authorization.substring(7)
     }
     return null
-}
+} */
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
@@ -17,12 +17,11 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', async (request, response) => {
+/* blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const token = getTokenFrom(request)
 
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })
     }
     const user = await User.findById(decodedToken.id)
@@ -45,6 +44,41 @@ blogsRouter.post('/', async (request, response) => {
         response.status(400).json({
             error: 'title or url missing'
         })
+    }
+}) */
+
+blogsRouter.post('/', async (request, response, next) => {
+    const body = request.body
+    /* const token = getTokenFrom(request) */
+
+    if (!body.title || !body.url) {
+        response.status(400).json({
+            error: 'title or url missing'
+        })
+    } else {
+        try {
+            const decodedToken = jwt.verify(request.token, process.env.SECRET)
+            if (!request.token || !decodedToken.id) {
+                return response.status(401).json({ error: 'token missing or invalid' })
+            }
+            const user = await User.findById(decodedToken.id)
+            const blog = new Blog({
+                title: body.title,
+                author: body.author,
+                url: body.url,
+                likes: body.likes || 0,
+                user: user.id
+            })
+
+            const savedBlog = await blog.save()
+            user.blogs = user.blogs.concat(savedBlog.id)
+            await user.save()
+
+            response.status(201).json(savedBlog)
+
+        } catch (exception) {
+            next(exception)
+        }
     }
 })
 
