868056e6ffe46ccefe947204f03a9beff2bc65af
pbrovkin <pbrovkin@gmail.com>
2020-09-07 22:05:03 +0300

    helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,14 +1,23 @@
 const dummy = (blogs) => {
-    if (blogs) {
-        return 1
-    }
+    return blogs ? 1 : 0
 }
 
 const totalLikes = (blogs) => {
     return blogs.map(blog => blog.likes).reduce((prev, curr) => prev + curr, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const favBlog = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
+    const result = {
+        title: favBlog.title,
+        author: favBlog.author,
+        likes: favBlog.likes
+    }
+    return result
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
