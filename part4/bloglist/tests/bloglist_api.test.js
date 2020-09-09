081c463ffb1e06058288853fa693497b609a4cff
pbrovkin <pbrovkin@gmail.com>
2020-09-09 17:54:17 +0300

    bloglist tests, step3

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -33,7 +33,6 @@ describe('when there is initially some blogs saved', () => {
 
     test('app returns the correct amount of blog posts', async () => {
         const response = await api.get('/api/blogs')
-
         expect(response.body).toHaveLength(initialBlogs.length)
     })
 
@@ -42,7 +41,37 @@ describe('when there is initially some blogs saved', () => {
         response.body.map(blog => expect(blog.id).toBeDefined())
     })
 
+    test('request creates a new blog post', async () => {
+        const newBlog = {
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+
+        expect(response.body).toHaveLength(initialBlogs.length + 1)
+
+        expect(response.body).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    title: "Canonical string reduction",
+                    author: "Edsger W. Dijkstra",
+                    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+                    likes: 12,
+                })
+            ])
+        )
+    })
 })
+
 
 afterAll(() => {
     mongoose.connection.close()
