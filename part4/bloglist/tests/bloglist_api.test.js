62ae5b76e8e0e6192b9c710083fb3c38ce181fe4
pbrovkin <pbrovkin@gmail.com>
2020-09-09 18:32:17 +0300

    bloglist tests, step4

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -70,8 +70,23 @@ describe('when there is initially some blogs saved', () => {
             ])
         )
     })
-})
 
+    test('likes property has 0 value by default if missing', async () => {
+        const newBlog = {
+            title: "First class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        }
+
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.likes).toEqual(0)
+    })
+})
 
 afterAll(() => {
     mongoose.connection.close()
