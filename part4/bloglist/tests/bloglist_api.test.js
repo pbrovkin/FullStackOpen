59b597d77b5e2faa544ac520976f5fab22be782a
pbrovkin <pbrovkin@gmail.com>
2020-09-10 19:12:27 +0300

    bloglist expansions, step1

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -10,12 +10,17 @@ describe('when there is initially some blogs saved', () => {
     beforeEach(async () => {
         await Blog.deleteMany({})
 
-        let blogObject = new Blog(helper.initialBlogs[0])
-        await blogObject.save()
-
-        blogObject = new Blog(helper.initialBlogs[1])
-        await blogObject.save()
+        const blogObjects = helper.initialBlogs
+            .map(blog => new Blog(blog))
+        const promiseArray = blogObjects.map(blog => blog.save())
+        await Promise.all(promiseArray)
     })
+
+    //the same with mongoose 'insertMany' method
+    /* beforeEach(async () => {
+        await Blog.deleteMany({})
+        await Blog.insertMany(helper.initialBlogs)
+    }) */
 
     test('app returns the correct amount of blog posts', async () => {
         const response = await api.get('/api/blogs')
@@ -27,66 +32,89 @@ describe('when there is initially some blogs saved', () => {
         response.body.map(blog => expect(blog.id).toBeDefined())
     })
 
-    test('request creates a new blog post', async () => {
-        const newBlog = {
-            title: 'Canonical string reduction',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-            likes: 12,
-        }
+    describe('addition of a new note', () => {
+        test('request creates a new blog post', async () => {
+            const newBlog = {
+                title: 'Canonical string reduction',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+                likes: 12,
+            }
 
-        await api
-            .post('/api/blogs')
-            .send(newBlog)
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
+            await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
 
-        const response = await api.get('/api/blogs')
+            const response = await api.get('/api/blogs')
 
-        expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+            expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
 
-        expect(response.body).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    title: 'Canonical string reduction',
-                    author: 'Edsger W. Dijkstra',
-                    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-                    likes: 12,
-                })
-            ])
-        )
+            expect(response.body).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        title: 'Canonical string reduction',
+                        author: 'Edsger W. Dijkstra',
+                        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+                        likes: 12,
+                    })
+                ])
+            )
+        })
+
+        test('likes property has 0 value by default if missing', async () => {
+            const newBlog = {
+                title: 'First class tests',
+                author: 'Robert C. Martin',
+                url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+            }
+
+            const response = await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
+
+            expect(response.body.likes).toEqual(0)
+        })
+
+        test('blog without title and url is not added', async () => {
+            const newBlog = {
+                author: 'Robert C. Martin',
+                likes: 10,
+            }
+
+            await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(400)
+
+            const blogsAtEnd = await helper.blogsInDb()
+
+            expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+        })
     })
 
-    test('likes property has 0 value by default if missing', async () => {
-        const newBlog = {
-            title: 'First class tests',
-            author: 'Robert C. Martin',
-            url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
-        }
+    describe('deletion of a blog', () => {
+        test('succeeds with status code 204 if id is valid', async () => {
+            const blogsAtStart = await helper.blogsInDb()
+            const blogToDelete = blogsAtStart[0]
 
-        const response = await api
-            .post('/api/blogs')
-            .send(newBlog)
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
+            await api
+                .delete(`/api/blogs/${blogToDelete.id}`)
+                .expect(204)
 
-        expect(response.body.likes).toEqual(0)
-    })
+            const blogsAtEnd = await helper.blogsInDb()
 
-    test('blog without title and url is not added', async () => {
-        const newBlog = {
-            author: 'Robert C. Martin',
-            likes: 10,
-        }
+            expect(blogsAtEnd.length).toBe(
+                helper.initialBlogs.length - 1
+            )
 
-        await api
-            .post('/api/blogs')
-            .send(newBlog)
-            .expect(400)
+            const titles = blogsAtEnd.map(b => b.title)
 
-        const blogsAtEnd = await helper.blogsInDb()
-
-        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+            expect(titles).not.toContain(blogToDelete.title)
+        })
     })
 })
 
--- part4/bloglist/tests/test_helper.js
+++ part4/bloglist/tests/test_helper.js
@@ -2,15 +2,15 @@ const Blog = require('../models/blog')
 
 const initialBlogs = [
     {
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
         likes: 7,
     },
     {
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
         likes: 5,
     }
 ]
