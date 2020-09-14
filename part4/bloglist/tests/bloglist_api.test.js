5f1b04886f2fe65ca16c465d7712897b701fc09c
pbrovkin <pbrovkin@gmail.com>
2020-09-14 17:57:01 +0300

    bloglist expansions, step5

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -183,7 +183,49 @@ describe('when there is initially some blogs saved', () => {
             expect(result.body.error).toContain('`username` to be unique')
 
             const usersAtEnd = await helper.usersInDb()
-            expect(usersAtEnd.length).toBe(usersAtStart.length)
+            expect(usersAtEnd).toHaveLength(usersAtStart.length)
+        })
+
+        test('creation fails with proper statuscode and message if username is less than 3 characters long', async () => {
+            const usersAtStart = await helper.usersInDb()
+
+            const newUser = {
+                username: 'te',
+                name: 'name',
+                password: 'password',
+            }
+
+            const result = await api
+                .post('/api/users')
+                .send(newUser)
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
+
+            expect(result.body.error).toContain(`is shorter than the minimum allowed length (3)`)
+
+            const usersAtEnd = await helper.usersInDb()
+            expect(usersAtEnd).toHaveLength(usersAtStart.length)
+        })
+
+        test('creation fails with proper statuscode and message if password is less than 3 characters long', async () => {
+            const usersAtStart = await helper.usersInDb()
+
+            const newUser = {
+                username: 'test',
+                name: 'name',
+                password: '',
+            }
+
+            const result = await api
+                .post('/api/users')
+                .send(newUser)
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
+
+            expect(result.body.error).toContain('password missing or must be at least 3 characters')
+
+            const usersAtEnd = await helper.usersInDb()
+            expect(usersAtEnd).toHaveLength(usersAtStart.length)
         })
     })
 })
