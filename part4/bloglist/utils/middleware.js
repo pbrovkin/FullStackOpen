c486b2a0960518d8c3186a0dc4082704956cf8d0
pbrovkin <pbrovkin@gmail.com>
2020-09-15 15:04:24 +0300

    bloglist expansions, step9

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -12,6 +12,14 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        request.token = authorization.substring(7)
+    }
+    next()
+}
+
 const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
         return response.status(400).send({
@@ -35,5 +43,6 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
     requestLogger,
     unknownEndpoint,
+    tokenExtractor,
     errorHandler
 }
