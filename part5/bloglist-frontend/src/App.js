eac730c050b6adf8feac0655c8fbf2445561346b
pbrovkin <pbrovkin@gmail.com>
2020-09-22 21:12:40 +0300

    blogilist frontend, step9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -106,7 +106,7 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Togglable>
       <div>
-        {blogs.map(blog =>
+        {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
           <Blog key={blog.id} blog={blog} addLike={addLike} />
         )}
       </div>
