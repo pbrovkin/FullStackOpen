93c0b9d7d70467f899a2705fab8df6f150eeed0b
pbrovkin <pbrovkin@gmail.com>
2020-09-25 14:06:36 +0300

    bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/integration/blogs_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blogs_app.spec.js
@@ -1,10 +1,35 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login from is shown', function () {
     cy.contains('Log in to application')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.contains('login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.contains('login').click()
+      cy.get('#username').type('mlu')
+      cy.get('#password').type('sal')
+      cy.get('#login-button').click()
+      cy.contains('invalid username or password')
+    })
   })
 })
