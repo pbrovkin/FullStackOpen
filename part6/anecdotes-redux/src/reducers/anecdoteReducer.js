1d9dfe2c94f7e795c01cae3f9b713b509f422750
pbrovkin <pbrovkin@gmail.com>
2020-09-30 11:51:01 +0300

    anecdotes, step1

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -22,8 +22,20 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
-
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.data.id
+      const anecdoteToVote = state.find(a => a.id === id)
+      const votedAnecdote = {
+        ...anecdoteToVote,
+        votes: anecdoteToVote.votes + 1
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : votedAnecdote
+      )
+    default:
+      return state
+  }
 }
 
 export default reducer
