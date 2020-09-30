64e3745d8305ccc87762d21c9ef0779fed353d81
pbrovkin <pbrovkin@gmail.com>
2020-09-30 17:12:47 +0300

    anecdotes, step7

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -12,8 +12,8 @@ const getId = () => (100000 * Math.random()).toFixed(0)
 const asObject = (anecdote) => {
   return {
     content: anecdote,
-    id: getId(),
-    votes: 0
+    votes: 0,
+    id: getId()
   }
 }
 
---
+++ part6/anecdotes-redux/src/store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import anecdoteReducer from './reducers/anecdoteReducer'
+
+const reducer = anecdoteReducer
+
+const store = createStore(
+  reducer,
+  composeWithDevTools()
+)
+
+export default store
