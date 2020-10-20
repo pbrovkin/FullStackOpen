005ef771ce2ebafe6e92410acb9b4cc42d3ad2fd
pbrovkin <pbrovkin@gmail.com>
2020-10-20 16:09:31 +0300

    remove

--- part7/blogs-frontend/src/store.js
+++
@@ -1,8 +0,0 @@
-import createStore from 'redux'
-import notificationReducer from './reducers/notificationReducer'
-
-const reducer = notificationReducer
-
-const store = createStore(reducer)
-
-export default store
