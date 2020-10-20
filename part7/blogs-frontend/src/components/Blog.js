59e4b1cd20aa88fb3c7bf85882c2de4ebc8b4e60
pbrovkin <pbrovkin@gmail.com>
2020-10-20 13:24:43 +0300

    initial frontend

---
+++ part7/blogs-frontend/src/components/Blog.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const Blog = ({ blog, handleLike, handleRemove, own }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const label = visible ? 'hide' : 'view'
+
+  return (
+    <div style={blogStyle} className='blog'>
+      <div>
+        <i>{blog.title}</i> by {blog.author} <button onClick={() => setVisible(!visible)}>{label}</button>
+      </div>
+      {visible && (
+        <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes}
+            <button onClick={() => handleLike(blog.id)}>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+          {own && <button onClick={() => handleRemove(blog.id)}>remove</button>}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Blog
