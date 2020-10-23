6b8e05336576c6f69777fb8e130412a7fec06133
pbrovkin <pbrovkin@gmail.com>
2020-10-23 13:37:24 +0300

    eslint errors fixed

---
+++ part7/blogs-frontend/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 12
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'react/prop-types': 0,
+    'indent': [
+      'error',
+      2,
+      { 'SwitchCase': 1 }
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
