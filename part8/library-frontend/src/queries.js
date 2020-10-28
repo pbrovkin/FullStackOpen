3c744bedd9a311b50ad749fbf419b8759682d2e4
pbrovkin <pbrovkin@gmail.com>
2020-10-28 11:51:07 +0200

    books view

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -3,9 +3,21 @@ import { gql } from '@apollo/client'
 export const ALL_AUTHORS = gql`
   query {
     allAuthors  {
+      id
       name
       born
       bookCount
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks  {
+      id
+      title
+      author
+      published
     }
   }
 `
