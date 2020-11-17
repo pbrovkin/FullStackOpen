22737d97c5594827043fee51cdc72a1dee1aae59
pbrovkin <pbrovkin@gmail.com>
2020-11-17 19:09:48 +0200

    command line

--- part9/first-steps/bmiCalculator.ts
+++ part9/first-steps/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface BmiParameters {
+  height: number;
+  weight: number;
+}
+
+const parseBmiArguments = (args: Array<string>): BmiParameters => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
 const calculateBmi = (height: number, mass: number) => {
   const heightInMeters = height / 100
   const bmi = mass / (heightInMeters * heightInMeters)
@@ -22,7 +41,8 @@ const calculateBmi = (height: number, mass: number) => {
 }
 
 try {
-  console.log(calculateBmi(180, 74))
+  const { height, weight } = parseBmiArguments(process.argv);
+  console.log(calculateBmi(height, weight))
 } catch (e) {
-  console.log('Something went wrong, error message: ', e.message);
+  console.log('Error, something bad happened, message: ', e.message);
 }
