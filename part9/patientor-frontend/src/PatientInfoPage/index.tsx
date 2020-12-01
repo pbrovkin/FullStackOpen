bb5a1973b604a6eb5a412d0bec670d855d01271d
pbrovkin <pbrovkin@gmail.com>
2020-12-01 17:56:37 +0200

    patientor, step5

--- part9/patientor-frontend/src/PatientInfoPage/index.tsx
+++ part9/patientor-frontend/src/PatientInfoPage/index.tsx
@@ -4,17 +4,32 @@ import { useStateValue } from "../state";
 import GenderIcon from "../components/GenderIcon"
 
 const PatientInfoPage: React.FC = () => {
-  const [{ patient }, dispatch] = useStateValue();
+  const [{ patient },] = useStateValue();
+
+  if (!patient) {
+    return <p>no data found</p>;
+  }
 
   return (
     <div>
       <h1>
-        {patient?.name}
+        {patient.name}
         {" "}
-        <GenderIcon gender={patient?.gender as Gender} />
+        <GenderIcon gender={patient.gender as Gender} />
       </h1>
-      <p>ssn: {patient?.ssn}</p>
-      <p>occupation: {patient?.occupation}</p>
+      <p>ssn: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
+      <h4>entries</h4>
+      {patient.entries.map(entry =>
+        <div key={entry.id}>
+          <p>{entry.date} {entry.description}</p>
+          <ul>
+            {entry.diagnosisCodes?.map(code =>
+              <li key={code}>{code}</li>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
--- part9/patientor-frontend/src/types.ts
+++ part9/patientor-frontend/src/types.ts
@@ -4,6 +4,52 @@ export interface Diagnosis {
   latin?: string;
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export enum Gender {
   Male = "male",
   Female = "female",
@@ -13,8 +59,9 @@ export enum Gender {
 export interface Patient {
   id: string;
   name: string;
-  occupation: string;
-  gender: Gender;
-  ssn?: string;
   dateOfBirth?: string;
+  ssn?: string;
+  gender: Gender;
+  occupation: string;
+  entries: Entry[];
 }
